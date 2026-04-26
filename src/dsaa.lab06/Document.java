8907e4649370d965be3859ec132f0678be06843e
ozio1212 <wiktorozi0501@gmail.com>
2026-04-26 18:47:15 +0200

    Implement Document, Link, and list functionality
    Fill in core functionality across Document, Link and TwoWayCycledOrderedListWithSentinel. Document: implement load() to parse lines and links, isCorrectId() and createLink() (parses optional positive weight), toString()/toStringReverse() formatting (10 per line), getWeights(), showArray(), and several sorting routines (bubble, insertion, selection, iterative merge with merge helper, and radix using countingSort). Link: add Objects import, equals() comparing ref, and compareTo() using lexicographical order. TwoWayCycledOrderedListWithSentinel: implement Element constructors and methods (addAfter, remove), iterator and list iterator, list constructor, add(E) preserving order via Comparable, element lookup by index and value, clear, contains, get, indexOf, isEmpty, remove by index/value, size, add(other) to merge another ordered list, removeAll(), and setSubOther() to remove elements present in another list. Overall implements missing behaviors required for parsing, sorting and ordered list operations.

--- src/dsaa.lab06/Document.java
+++ src/dsaa.lab06/Document.java
@@ -12,74 +12,298 @@ public class Document{
 		load(scan);
 	}
 	public void load(Scanner scan) {
-		//TODO
+		while(scan.hasNextLine()) {
+
+			String line = scan.nextLine();
+
+			// end of a document
+			if(line.equals("eod")){
+				break;
+			}
+
+			// rozdzielenie słów do tablicy
+			// białe znaki, a znak \s oznacza dowolny biały znak, a + oznacza jeden lub więcej.
+			String[] words = line.split("\\s+");
+
+			// szukanie linków i sprawdzanie poprawnosci
+			for (String word : words) {
+				word = word.toLowerCase();
+				if(word.startsWith("link=")) {
+					String link = word.substring(5);
+					Link parsedLink = createLink(link);
+
+					if(parsedLink!=null) {
+						// dodanie linku do listy zamiast wyprintowanie jak na liscie 1
+						this.link.add(parsedLink);
+					}
+				}
+			}
+		}
 	}
-	
-	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	
 
 	public static boolean isCorrectId(String id) {
-		//TODO
-		return false;
+		if (id == null || id.length() == 0 || !Character.isLetter(id.charAt(0))) {
+			return false;
+		}
+		for (int i = 1; i < id.length(); i++) {
+			if(!Character.isLetterOrDigit(id.charAt(i)) && id.charAt(i) != '_') {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	public static Link createLink(String link) {
-		//TODO
-		return null;
+	static Link createLink(String link) {
+
+		int openBracket = link.indexOf('(');
+
+		if (openBracket == -1) {
+			if (isCorrectId(link)) {
+				return new Link(link);
+			}
+			return null;
+		} else {
+
+			String linkID = link.substring(0, openBracket);
+
+			int closeBracket = link.indexOf(')',openBracket);
+
+			if (closeBracket != -1 && isCorrectId(linkID)) {
+				String weightString = link.substring(openBracket + 1, closeBracket);
+
+				try {
+					int weight = Integer.parseInt(weightString);
+					if (weight > 0) {
+						return new Link(linkID, weight);
+					}
+				} catch (NumberFormatException e) {
+					return null;
+				}
+			}
+			return null;
+		}
 	}
 
 	@Override
 	public String toString() {
-		String retStr="Document: "+name;
-		//TODO
-		return retStr;
+		StringBuilder sb = new StringBuilder();
+		sb.append("Document: ").append(name);
+
+		int count = 0;
+
+		for (Link link : link) {
+			if (count % 10 == 0){
+				sb.append("\n");
+			} else {
+				sb.append(" ");
+			}
+
+			sb.append(link.toString());
+			count++;
+		}
+
+		return sb.toString();
 	}
 
 	public String toStringReverse() {
-		String retStr="Document: "+name;
+		StringBuilder sb = new StringBuilder();
+		sb.append("Document: ").append(name);
+
 		ListIterator<Link> iter=link.listIterator();
-		while(iter.hasNext())
+
+		while(iter.hasNext()) {
 			iter.next();
-		//TODO
-		while(iter.hasPrevious()){
 		}
-		return retStr;
+
+		int count = 0;
+
+		while (iter.hasPrevious()) {
+			if (count % 10 == 0){
+				sb.append("\n");
+			} else {
+				sb.append(" ");
+			}
+			sb.append(iter.previous().toString());
+			count++;
+		}
+
+		return sb.toString();
 	}
+
+
 	public int[] getWeights() {
-		//TODO
-		return null;
+		int[] weights = new int[link.size()];
+		int index = 0;
+
+		for(Link link : link) {
+			weights[index] = link.weight;
+			index++;
+		}
+
+		return weights;
 	}
 
 	public static void showArray(int[] arr) {
-		// TODO
+		if (arr==null || arr.length==0) {
+			return;
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < arr.length; i++) {
+			sb.append(arr[i]);
+			if (i < arr.length-1) {
+				sb.append(" ");
+			}
+		}
+
+		System.out.println(sb.toString());
 	}
 
 	void bubbleSort(int[] arr) {
 		showArray(arr);
-		//TODO
+
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++){
+
+			for(int j = n-1; j > i; j--){
+
+				if(arr[j] < arr[j-1]){
+					int temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				}
+
+			}
+			showArray(arr);
+		}
 	}
 
 	public void insertSort(int[] arr) {
 		showArray(arr);
-		//TODO
-		
+
+		int n = arr.length;
+
+		for (int i = n - 2; i >= 0 ; i--){
+
+			int key = arr[i];
+			int j = i + 1;
+
+			while (j < n && arr[j] < key){
+				arr[j-1] = arr[j];
+				j++;
+			}
+
+			arr[j-1] = key;
+
+			showArray(arr);
+		}
 	}
 	public void selectSort(int[] arr) {
 		showArray(arr);
-		//TODO
+
+		int n = arr.length;
+
+		for (int i = n-1; i>0; i--){
+
+			int maxIndex = 0;
+
+			for (int j = 1; j <= i; j++){
+				if(arr[j] > arr[maxIndex]){
+					maxIndex = j;
+				}
+			}
+
+			int temp = arr[i];
+			arr[i] = arr[maxIndex];
+			arr[maxIndex] = temp;
+
+			showArray(arr);
+		}
 	}
-	
 
 	public void iterativeMergeSort(int[] arr) {
 		showArray(arr);
-		//TODO
+
+		int n = arr.length;
+		int[] temp = new int[n];
+
+		for (int currSize = 1; currSize < n; currSize *= 2) {
+
+			for (int leftStart = 0; leftStart < n - 1; leftStart += 2 * currSize) {
+
+				int mid = Math.min(leftStart + currSize - 1, n - 1);
+				int rightEnd = Math.min(leftStart + 2 * currSize - 1, n - 1);
+
+				merge(arr, temp, leftStart, mid, rightEnd);
+			}
+
+			showArray(arr);
+		}
 	}
 
 	public void radixSort(int[] arr) {
 		showArray(arr);
-		//TODO
+
+		for (int exp = 1; exp <= 100; exp *= 10) {
+			countingSort(arr, exp);
+
+			showArray(arr);
+		}
 	}
 
+	private void merge(int[] arr, int[] temp, int left, int mid, int right) {
+		int i = left;
+		int j = mid + 1;
+		int k = left;
 
+		while (i <= mid && j <= right) {
+			if (arr[i] <= arr[j]) {
+				temp[k++] = arr[i++];
+			} else {
+				temp[k++] = arr[j++];
+			}
+		}
+
+		while (i <= mid) {
+			temp[k++] = arr[i++];
+		}
+
+		while (j <= right) {
+			temp[k++] = arr[j++];
+		}
+
+		for (i = left; i <= right; i++) {
+			arr[i] = temp[i];
+		}
+	}
+
+	private void countingSort(int[] arr, int exp) {
+		int n = arr.length;
+		int[] output = new int[n];
+		int[] count = new int[10];
+
+		for (int i = 0; i < n; i++) {
+			int digit = (arr[i] / exp) % 10;
+			count[digit]++;
+		}
+
+		for (int i = 1; i < 10; i++) {
+			count[i] += count[i - 1];
+		}
+
+		for (int i = n - 1; i >= 0; i--) {
+			int digit = (arr[i] / exp) % 10;
+
+			output[count[digit] - 1] = arr[i];
+
+			count[digit]--;
+		}
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = output[i];
+		}
+	}
 }
--- src/dsaa.lab06/TwoWayCycledOrderedListWithSentinel.java
+++ src/dsaa.lab06/TwoWayCycledOrderedListWithSentinel.java
@@ -2,141 +2,194 @@ package dsaa.lab06;
 
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
+public class TwoWayCycledOrderedListWithSentinel<E> implements IList<E> {
 
-public class TwoWayCycledOrderedListWithSentinel<E> implements IList<E>{
-
-	private class Element{
+	private class Element {
 		public Element(E e) {
-			//TODO
+			this.object = e;
 		}
+
 		public Element(E e, Element next, Element prev) {
-			//TODO
+			this.object = e;
+			this.next = next;
+			this.prev = prev;
 		}
+
 		// add element e after this
 		public void addAfter(Element elem) {
-			//TODO
+			elem.next = this.next;
+			elem.prev = this;
+			this.next.prev = elem;
+			this.next = elem;
 		}
+
 		// assert it is NOT a sentinel
 		public void remove() {
-			//TODO
+			if (this == sentinel) {
+				return;
+			}
+			this.prev.next = this.next;
+			this.next.prev = this.prev;
 		}
+
 		E object;
-		Element next=null;
-		Element prev=null;
+		Element next = null;
+		Element prev = null;
 	}
 
-
+	// pola listy
 	Element sentinel;
 	int size;
 
-	private class InnerIterator implements Iterator<E>{
-		//TODO
+	private class InnerIterator implements Iterator<E> {
+
+		Element pos;
+
 		public InnerIterator() {
-			//TODO
+			this.pos = sentinel.next;
 		}
+
 		@Override
 		public boolean hasNext() {
-			//TODO
-			return false;
+			return pos != sentinel;
 		}
 
 		@Override
 		public E next() {
-			//TODO
-			return null;
+			if (!hasNext()) throw new NoSuchElementException();
+			E value = pos.object;
+			pos = pos.next;
+			return value;
 		}
 	}
 
-	private class InnerListIterator implements ListIterator<E>{
-		//TODO
+	private class InnerListIterator implements ListIterator<E> {
+
+		Element current;
+
 		public InnerListIterator() {
-			//TODO
+			this.current = sentinel.next;
 		}
+
 		@Override
 		public boolean hasNext() {
-			//TODO
-			return false;
+			return current != sentinel;
 		}
 
 		@Override
 		public E next() {
-			//TODO
-			return null;
+			if (!hasNext()) throw new NoSuchElementException();
+			E value = current.object;
+			current = current.next;
+			return value;
 		}
+
 		@Override
 		public void add(E arg0) {
 			throw new UnsupportedOperationException();
 		}
+
 		@Override
 		public boolean hasPrevious() {
-			//TODO
-			return false;
+			return current.prev != sentinel;
 		}
+
 		@Override
 		public int nextIndex() {
 			throw new UnsupportedOperationException();
 		}
+
 		@Override
 		public E previous() {
-			//TODO
-			return null;
+			if (!hasPrevious()) throw new NoSuchElementException();
+			current = current.prev;
+			return current.object;
 		}
+
 		@Override
 		public int previousIndex() {
 			throw new UnsupportedOperationException();
 		}
+
 		@Override
 		public void remove() {
 			throw new UnsupportedOperationException();
 		}
+
 		@Override
 		public void set(E arg0) {
 			throw new UnsupportedOperationException();
 		}
 	}
+
 	public TwoWayCycledOrderedListWithSentinel() {
-		//TODO
+		sentinel = new Element(null);
+		sentinel.next = sentinel;
+		sentinel.prev = sentinel;
+		size = 0;
 	}
 
-	//@SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean add(E e) {
-		//TODO
-		return false;
+		Comparable<E> comparable = (Comparable<E>) e;
+		Element current = sentinel.next;
+
+		while (current != sentinel && comparable.compareTo(current.object) >= 0) {
+			current = current.next;
+		}
+
+		Element newElement = new Element(e);
+		current.prev.addAfter(newElement);
+		size++;
+		return true;
 	}
 
 	private Element getElement(int index) {
-		//TODO
-		return null;
+		if (index < 0 || index >= size) {
+			throw new NoSuchElementException();
+		}
+		Element current = sentinel.next;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+		}
+		return current;
 	}
 
 	private Element getElement(E obj) {
-		//TODO
+		Element current = sentinel.next;
+
+		while (current != sentinel) {
+			if (current.object.equals(obj)) {
+				return current;
+			}
+			current = current.next;
+		}
 		return null;
 	}
 
 	@Override
 	public void add(int index, E element) {
 		throw new UnsupportedOperationException();
-
 	}
 
 	@Override
 	public void clear() {
-		//TODO
+		sentinel.next = sentinel;
+		sentinel.prev = sentinel;
+		size = 0;
 	}
 
 	@Override
 	public boolean contains(E element) {
-		//TODO
-		return false;
+		return indexOf(element) != -1;
 	}
 
 	@Override
 	public E get(int index) {
-		//TODO
-		return null;
+		return getElement(index).object;
 	}
 
 	@Override
@@ -146,14 +199,22 @@ public class TwoWayCycledOrderedListWithSentinel<E> implements IList<E>{
 
 	@Override
 	public int indexOf(E element) {
-		//TODO
+		Element current = sentinel.next;
+		int index = 0;
+
+		while (current != sentinel) {
+			if (current.object.equals(element)) {
+				return index;
+			}
+			current = current.next;
+			index++;
+		}
 		return -1;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		//TODO
-		return true;
+		return size == 0;
 	}
 
 	@Override
@@ -168,31 +229,104 @@ public class TwoWayCycledOrderedListWithSentinel<E> implements IList<E>{
 
 	@Override
 	public E remove(int index) {
-		//TODO
-		return null;
+		Element current = getElement(index);
+		current.remove();
+		size--;
+		return current.object;
 	}
 
 	@Override
 	public boolean remove(E e) {
-		//TODO
+		Element current = getElement(e);
+		if (current != null) {
+			current.remove();
+			size--;
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public int size() {
-		//TODO
-		return -1;
+		return size;
 	}
 
-	//@SuppressWarnings("unchecked")
 	public void add(TwoWayCycledOrderedListWithSentinel<E> other) {
-		//TODO
-	}
-	
-	//@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void removeAll(E e) {
-		//TODO
+
+		if (this == other || other.isEmpty()) return;
+
+		Element myCurrent = this.sentinel.next;
+		Element otherCurrent = other.sentinel.next;
+
+		while (otherCurrent != other.sentinel) {
+			Element nextOther = otherCurrent.next;
+			Comparable<E> comparable = (Comparable<E>) otherCurrent.object;
+
+			while (myCurrent != this.sentinel && comparable.compareTo(myCurrent.object) >= 0) {
+				myCurrent = myCurrent.next;
+			}
+
+			myCurrent.prev.addAfter(otherCurrent);
+			this.size++;
+
+			otherCurrent = nextOther;
+
+		}
+
+		other.clear();
 	}
 
+	public void removeAll(E e) {
+		Element current = sentinel.next;
+
+		while (current != sentinel) {
+			Element nextElement = current.next;
+
+			if (current.object.equals(e)) {
+				current.remove();
+				size--;
+			}
+
+			current = nextElement;
+		}
+	}
+
+	public void setSubOther(TwoWayCycledOrderedListWithSentinel<E> other) {
+
+		// te same listy
+		if (this == other) {
+			this.clear();
+			return;
+		}
+
+		// obie puste
+		if (this.isEmpty() || other.isEmpty()) return;
+
+		Element current = this.sentinel.next;
+		Element otherCurrent = other.sentinel.next;
+
+		while (current != this.sentinel && otherCurrent != other.sentinel) {
+
+			Comparable<E> otherComp = (Comparable<E>) otherCurrent.object;
+			int comp = otherComp.compareTo(current.object);
+
+			if (comp > 0) {
+				current = current.next;
+
+			} else if (comp < 0) {
+				otherCurrent = otherCurrent.next;
+
+			} else {
+				// comp = 0 -> te same elementy
+				Element next = current.next;
+				current.remove();
+				this.size--;
+
+				current = next;
+				otherCurrent = otherCurrent.next;
+			}
+		}
+	}
 }
+
 
