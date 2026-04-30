15cc14b8f270bfc6f61691982f760cc2c7e7e5bf
ozio1212 <wiktorozi0501@gmail.com>
2026-04-30 17:54:21 +0200

    feat: add list 7

--- src/dsaa.lab07/Document.java
+++ src/dsaa.lab07/Document.java
@@ -4,10 +4,16 @@ import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Document implements IWithName{
+
 	public String name;
 	public TwoWayCycledOrderedListWithSentinel<Link> link;
+
+	private static final int MODVALUE = 100000000;
+	private static final int[] SEQUENCE = {7, 11, 13, 17, 19};
+
 	public Document(String name) {
-		//TODO
+		this.name = name.toLowerCase();
+		link = new TwoWayCycledOrderedListWithSentinel<Link>();
 	}
 
 	public Document(String name, Scanner scan) {
@@ -15,48 +21,156 @@ public class Document implements IWithName{
 		link=new TwoWayCycledOrderedListWithSentinel<Link>();
 		load(scan);
 	}
+
 	public void load(Scanner scan) {
-		//TODO
+		while(scan.hasNextLine()) {
+
+			String line = scan.nextLine();
+
+			// end of a document
+			if(line.equals("eod")){
+				break;
+			}
+
+			// rozdzielenie słów do tablicy
+			// białe znaki, a znak \s oznacza dowolny biały znak, a + oznacza jeden lub więcej.
+			String[] words = line.split("\\s+");
+
+			// szukanie linków i sprawdzanie poprawnosci
+			for (String word : words) {
+				word = word.toLowerCase();
+				if(word.startsWith("link=")) {
+					String link = word.substring(5);
+					Link parsedLink = createLink(link);
+
+					if(parsedLink!=null) {
+						// dodanie linku do listy zamiast wyprintowanie jak na liscie 1
+						this.link.add(parsedLink);
+					}
+				}
+			}
+		}
 	}
-	
-	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	
 
 	public static boolean isCorrectId(String id) {
-		//TODO
-		return false;
+		if (id == null || id.length() == 0 || !Character.isLetter(id.charAt(0))) {
+			return false;
+		}
+		for (int i = 1; i < id.length(); i++) {
+			if(!Character.isLetterOrDigit(id.charAt(i)) && id.charAt(i) != '_') {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	public static Link createLink(String link) {
-		//TODO
-		return null;
+	static Link createLink(String link) {
+
+		int openBracket = link.indexOf('(');
+
+		if (openBracket == -1) {
+			if (isCorrectId(link)) {
+				return new Link(link);
+			}
+			return null;
+		} else {
+
+			String linkID = link.substring(0, openBracket);
+
+			int closeBracket = link.indexOf(')',openBracket);
+
+			if (closeBracket != -1 && isCorrectId(linkID)) {
+				String weightString = link.substring(openBracket + 1, closeBracket);
+
+				try {
+					int weight = Integer.parseInt(weightString);
+					if (weight > 0) {
+						return new Link(linkID, weight);
+					}
+				} catch (NumberFormatException e) {
+					return null;
+				}
+			}
+			return null;
+		}
 	}
 
 	@Override
 	public String toString() {
-		String retStr="Document: "+name;
-		//TODO
-		return retStr;
+		StringBuilder sb = new StringBuilder();
+		sb.append("Document: ").append(name);
+
+		int count = 0;
+
+		for (Link link : link) {
+			if (count % 10 == 0){
+				sb.append("\n");
+			} else {
+				sb.append(" ");
+			}
+
+			sb.append(link.toString());
+			count++;
+		}
+
+		return sb.toString();
 	}
 
 	public String toStringReverse() {
-		String retStr="Document: "+name;
+		StringBuilder sb = new StringBuilder();
+		sb.append("Document: ").append(name);
+
 		ListIterator<Link> iter=link.listIterator();
-		while(iter.hasNext())
+
+		while(iter.hasNext()) {
 			iter.next();
-		//TODO
-		while(iter.hasPrevious()){
 		}
-		return retStr;
+
+		int count = 0;
+
+		while (iter.hasPrevious()) {
+			if (count % 10 == 0){
+				sb.append("\n");
+			} else {
+				sb.append(" ");
+			}
+			sb.append(iter.previous().toString());
+			count++;
+		}
+
+		return sb.toString();
 	}
 
 	@Override
 	public String getName() {
-		// TODO
-		return null;
+		return name;
 	}
 
+	@Override
+	public boolean equals(Object object){
+		if (this == object) return true;
+		if (object == null || getClass() != object.getClass()) return  false;
+		Document other = (Document) object;
+		return name.equals(other.name);
+	}
 
+	@Override
+	public int hashCode(){
+		if (name == null || name.isEmpty()){
+			return 0;
+		}
+
+		int hash = name.charAt(0);
+
+		for (int i = 1 ; i < name.length() ; i++){
+			int seqIndex = (i-1) % SEQUENCE.length;
+			int multiplier = SEQUENCE[seqIndex];
+
+			hash = ( ( (hash*multiplier) + name.charAt(i) ) % MODVALUE);
+		}
+
+		return hash;
+	}
 }
 
--- src/dsaa.lab07/HashTable.java
+++ src/dsaa.lab07/HashTable.java
@@ -8,39 +8,103 @@ public class HashTable{
 	private final static double defaultMaxLoadFactor=0.7;
 	private int size;	
 	private final double maxLoadFactor;
+
 	public HashTable() {
 		this(defaultInitSize);
 	}
+
 	public HashTable(int size) {
 		this(size,defaultMaxLoadFactor);
 	}
 
-
 	public HashTable(int initCapacity, double maxLF) {
-		//TODO
 		this.maxLoadFactor=maxLF;
+		this.size = 0;
+		this.arr = new LinkedList[initCapacity];
+
+		for (int i = 0; i<initCapacity; i++){
+			this.arr[i] = new LinkedList();
+		}
 	}
 
 	public boolean add(Object elem) {
-		//TODO
+
+		int hashIndex = Math.abs(elem.hashCode()) % arr.length;
+
+		LinkedList list = arr[hashIndex];
+
+		if (list.contains(elem)){
+			return false;
+		}
+
+		list.add(elem);
+		size++;
+
+		double currentLoadFactor = (double) size / arr.length;
+		if (currentLoadFactor > maxLoadFactor) {
+			doubleArray();
+		}
+
 		return true;
 	}
 
 	
 	private void doubleArray() {
-		//TODO
+		int newCapacity = arr.length * 2;
+		LinkedList[] newArr = new LinkedList[newCapacity];
+
+		for (int i = 0; i < newCapacity; i++){
+			newArr[i] = new LinkedList();
+		}
+
+		for (int i = 0; i < arr.length; i++){
+			for (Object e : arr[i]){
+				int hashIndex = Math.abs(e.hashCode()) % newCapacity;
+				newArr[hashIndex].add(e);
+			}
+		}
+
+		this.arr = newArr;
 	}
 
 
 	@Override
 	public String toString() {
-		//TODO
-		// use	IWithName x=(IWithName)elem;
-		return null;
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < arr.length; i++){
+			sb.append(i).append(":");
+
+			if (!arr[i].isEmpty()){
+				sb.append(" ");
+				boolean isFirst = true;
+
+				for (Object elem : arr[i]){
+					if (!isFirst){
+						sb.append(", ");
+					}
+
+					IWithName doc = (IWithName) elem;
+					sb.append(doc.getName());
+
+					isFirst=false;
+				}
+			}
+			sb.append("\n");
+		}
+
+		return sb.toString();
 	}
 
 	public Object get(Object toFind) {
-		//TODO
+		int hashIndex = Math.abs(toFind.hashCode()) % arr.length;
+		LinkedList list = arr[hashIndex];
+
+		for (Object elem : list) {
+			if (elem.equals(toFind)) {
+				return elem;
+			}
+		}
 		return null;
 	}
 	
